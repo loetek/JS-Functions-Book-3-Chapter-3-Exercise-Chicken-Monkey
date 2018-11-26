8af7720f87b3e3e0b228fbac72dd0bc250a403d1
JD <jdwheeler0@gmail.com>
2018-11-26 15:35:21 -0600

    done

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 for (let currentNumber = 1; currentNumber <= 100; currentNumber++) {
-    // Divide the current number by 2, and check if the remainder is 0
-    if (currentNumber % 2 === 0) {
-        console.log(currentNumber) // Only 2, 4, 6 will appear
+
+
+    if (currentNumber % 5 === 0) {
+        let chicken = 'chicken';
+        console.log(chicken) 
+    }
+    else if(currentNumber % 7 === 0){
+        let monkey = 'monkey';
+        console.log(monkey) 
+
+    }
+    else{
+        console.log(currentNumber);
+
+
     }
 }
